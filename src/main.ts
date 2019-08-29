604f0b525e5cb34c37ab3333098dfc4ab0f1389b
remi-debette <remi.debette@axa.fr>
2019-08-29 19:14:48 +0200

    Init commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import * as core from '@actions/core';
+import { exec as _exec } from 'child_process';
+import { promisify } from 'util';
+
+const exec = promisify(_exec);
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const MAJEURE = core.getInput('majeure');
+    core.exportVariable('MAJEURE', MAJEURE);
+
+    const { stdout, stderr } = await exec("$MAJEURE.$(date +%m-%d).$(date -d '+1 hour' '+%H%M%S')");
+
+    core.exportVariable('VERSION', stdout);
   } catch (error) {
     core.setFailed(error.message);
   }
